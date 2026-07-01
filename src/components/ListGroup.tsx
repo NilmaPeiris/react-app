cd73c132af6950936e85a6fa05ca03c431ce71b9
NilmaPeiris <nilmapeiris10@gmail.com>
2026-07-02 00:57:16 +1200

    created a group list

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+function ListGroup() {
+  const items = ["Nilma", "Saku", "Kandy", "Kalutara"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;

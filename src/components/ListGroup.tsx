c924e9536a2bb44a5e3aea52ab0fab53fe434a60
NilmaPeiris <nilmapeiris10@gmail.com>
2026-07-02 01:10:28 +1200

    Click items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,19 @@
 function ListGroup() {
-  const items = ["Nilma", "Saku", "Kandy", "Kalutara"];
+  let items = ["Nilma", "Saku", "Kandy", "Kalutara"];
 
   return (
     <>
       <h1>List</h1>
+      {items.length == 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>

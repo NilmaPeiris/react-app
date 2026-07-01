ba77af08e6e4a9c40ef79a98a1dbc3c0e7dd877d
NilmaPeiris <nilmapeiris10@gmail.com>
2026-07-02 01:18:39 +1200

    event handlers

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,17 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Nilma", "Saku", "Kandy", "Kalutara"];
 
+  //event handler
+  const handleClicked = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {items.length == 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log(item)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClicked}>
             {item}
           </li>
         ))}
